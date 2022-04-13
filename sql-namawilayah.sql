1449faa4f90b7e5a97b26c8fbf1046234228ce74
rusdigani <94724259+rusdigani@users.noreply.github.com>
2022-04-14 00:46:35 +0700

    Add files via upload

---
+++ sql-namawilayah.sql
@@ -0,0 +1,102 @@
+Updatecheck<|||>3<|||>1
+DoUsageStatistics<|||>3<|||>1
+UpdatecheckLastrun<|||>1<|||>2022-04-13 12:58:02
+LastUsageStatisticCall<|||>1<|||>2022-04-01 20:36:45
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
+ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,1
+MainWinOnMonitor<|||>3<|||>0
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,263
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,428,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,10,50,50,50,50,50,50,50,50,50,50
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,203
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+LastSessions<|||>1<|||>loca host
+LastActiveSession<|||>1<|||>loca host
+SessionManager_WindowLeft<|||>3<|||>335
+SessionManager_WindowTop<|||>3<|||>83
+CreateDbCollation<|||>1<|||>latin1_swedish_ci
+ColWidths_frmTableEditor.listColumns<|||>1<|||>38,100,154,90,60,65,50,117,130,140,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>434,100,80
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>200,80,100,80,80,80
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,420
+ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
+querymemoheight<|||>3<|||>230
+UpdateCheckWindowWidth<|||>3<|||>1382
+UpdateCheckWindowHeight<|||>3<|||>784
+MemoEditorWidth<|||>3<|||>136
+DateTimeEditor_CursorPos_Type4<|||>3<|||>19
+Servers\loca host\SessionCreated<|||>1<|||>2022-04-02 14:02:46
+Servers\loca host\Host<|||>1<|||>127.0.0.1
+Servers\loca host\WindowsAuth<|||>3<|||>0
+Servers\loca host\CleartextPluginEnabled<|||>3<|||>0
+Servers\loca host\User<|||>1<|||>root
+Servers\loca host\Password<|||>1<|||>6
+Servers\loca host\LoginPrompt<|||>3<|||>0
+Servers\loca host\Port<|||>1<|||>3306
+Servers\loca host\NetType<|||>3<|||>0
+Servers\loca host\Compressed<|||>3<|||>0
+Servers\loca host\LocalTimeZone<|||>3<|||>0
+Servers\loca host\QueryTimeout<|||>3<|||>30
+Servers\loca host\KeepAlive<|||>3<|||>20
+Servers\loca host\FullTableStatus<|||>3<|||>1
+Servers\loca host\Databases<|||>1<|||>mysql;latihan_oop;nama_wiayah
+Servers\loca host\Library<|||>1<|||>libmariadb.dll
+Servers\loca host\Comment<|||>1<|||>
+Servers\loca host\StartupScriptFilename<|||>1<|||>
+Servers\loca host\TreeBackground<|||>3<|||>536870911
+Servers\loca host\SSHtunnelHost<|||>1<|||>
+Servers\loca host\SSHtunnelHostPort<|||>3<|||>22
+Servers\loca host\SSHtunnelUser<|||>1<|||>
+Servers\loca host\SSHtunnelPassword<|||>1<|||>9
+Servers\loca host\SSHtunnelTimeout<|||>3<|||>4
+Servers\loca host\SSHtunnelPrivateKey<|||>1<|||>
+Servers\loca host\SSHtunnelPort<|||>3<|||>3307
+Servers\loca host\SSL_Active<|||>3<|||>0
+Servers\loca host\SSL_Key<|||>1<|||>
+Servers\loca host\SSL_Cert<|||>1<|||>
+Servers\loca host\SSL_CA<|||>1<|||>
+Servers\loca host\SSL_Cipher<|||>1<|||>
+Servers\loca host\IgnoreDatabasePattern<|||>1<|||>
+Servers\loca host\LogFileDdl<|||>3<|||>0
+Servers\loca host\LogFileDml<|||>3<|||>0
+Servers\loca host\LogFilePath<|||>1<|||>C:\Users\abdurrauf h. a. gani\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\loca host\ServerVersionFull<|||>1<|||>10.4.22-MariaDB - mariadb.org binary distribution
+Servers\loca host\ConnectCount<|||>3<|||>3
+Servers\loca host\ServerVersion<|||>3<|||>100422
+Servers\loca host\LastConnect<|||>1<|||>2022-04-13 14:07:54
+Servers\loca host\lastUsedDB<|||>1<|||>nama_wiayah
+Servers\loca host\QueryHistory\1<|||>1<|||>2022-04-02 14:14:29|latihan_oop|641|CREATE TABLE tbl_users (<{{{><}}}>	user_id INT(11) NOT NULL AUTO_INCREMENT,<{{{><}}}>	user_name VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',<{{{><}}}>	user_password VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',<{{{><}}}>	user_mail VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',<{{{><}}}>	unique_id VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',<{{{><}}}>	user_created DATETIME NOT NULL DEFAULT current_timestamp(),<{{{><}}}>	PRIMARY KEY (user_id) USING BTREE,<{{{><}}}>	UNIQUE INDEX user_name (user_name) USING BTREE<{{{><}}}>)<{{{><}}}>COLLATE='utf8mb4_general_ci'<{{{><}}}>ENGINE=INNODB<{{{><}}}>AUTO_INCREMENT=1<{{{><}}}>;
+Servers\loca host\QueryHistory\2<|||>1<|||>2022-04-13 14:12:33|nama_wiayah|719|CREATE TABLE tbl_provinsi (<{{{><}}}>id_provinsi INT(11) NOT NULL AUTO_INCREMENT,<{{{><}}}>kode_provinsi VARCHAR(26) NOT NULL,<{{{><}}}>created_at DATETIME DEFAULT CURRENT_TIMESTAMP(),<{{{><}}}>updated_at DATETIME NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(),<{{{><}}}>nama_provinsi VARCHAR(26) NOT NULL,<{{{><}}}>PRIMARY KEY (id_provinsi) USING BTREE,<{{{><}}}>	UNIQUE INDEX kode_provinsi (kode_provinsi) USING BTREE<{{{><}}}>)<{{{><}}}>ENGINE=InnoDB<{{{><}}}>AUTO_INCREMENT=1<{{{><}}}>;
+Servers\loca host\QueryHistory\3<|||>1<|||>2022-04-13 14:13:34|nama_wiayah|406|CREATE TABLE tbl_kabkota (<{{{><}}}>id_kabkota INT(11) NOT NULL AUTO_INCREMENT,<{{{><}}}>kode_kabkota VARCHAR(26) NOT NULL,<{{{><}}}>created_at DATETIME DEFAULT CURRENT_TIMESTAMP(),<{{{><}}}>updated_at DATETIME NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(),<{{{><}}}>nama_kabkota  VARCHAR(50) NOT NULL,<{{{><}}}>kode_provinsi VARCHAR(26) NOT NULL,<{{{><}}}>PRIMARY KEY (id_kabkota) USING BTREE,<{{{><}}}>  UNIQUE INDEX kode_kabkota (kode_kabkota) USING BTREE<{{{><}}}> <{{{><}}}>)<{{{><}}}>ENGINE=InnoDB<{{{><}}}>AUTO_INCREMENT=1<{{{><}}}>;
